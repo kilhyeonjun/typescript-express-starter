3beac8623ed9a6c2521fa35c39bd22408d9a4bf6
kilhyeonjun <kboxstar@gmail.com>
2023-06-25 22:11:44 +0900

    add: knex migrations

---
+++ db/migrations/20230625125738_create_user.js
@@ -0,0 +1,21 @@
+const { table } = require("console");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id"); // Primary key auto increments
+    table.string("first_name").notNullable(); // not null field
+    table.string("last_name").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
