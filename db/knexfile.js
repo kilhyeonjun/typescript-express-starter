d3761819c311669ca99fe93a5049a289f19ec219
kilhyeonjun <kboxstar@gmail.com>
2023-06-25 22:11:18 +0900

    add: knexfile

---
+++ db/knexfile.js
@@ -0,0 +1,24 @@
+require("dotenv").config({ path: "../.env" });
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "test_db",
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_user_migrations",
+    },
+  },
+};
